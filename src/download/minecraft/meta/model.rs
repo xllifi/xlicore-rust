ba20de5e3111dde3c9fa5ac210414c9b374e447c
xllifi <unicoded@ya.ru>
2025-05-15 22:54:19 +1000

    cloneable minecraft meta structs

--- src/download/minecraft/meta/model.rs
+++ src/download/minecraft/meta/model.rs
@@ -3,18 +3,18 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 /// Struct for https://piston-meta.mojang.com/mc/game/version_manifest_v2.json
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftMetaVersionManifest {
   pub latest: MinecraftMetaVersionManifestLatest,
   pub versions: Vec<MinecraftMetaVersionManifestPackage>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftMetaVersionManifestLatest {
   pub release: String,
   pub snapshot: String,
 }
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftMetaVersionManifestPackage {
   pub id: String,
   #[serde(rename = "type")]
@@ -29,7 +29,7 @@ pub struct MinecraftMetaVersionManifestPackage {
 }
 
 /// Structs for https://piston-meta.mojang.com/v1/packages/<hash>/<version>.json
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifest {
   pub arguments: MinecraftPackageManifestArguments,
   #[serde(rename = "assetIndex")]
@@ -54,51 +54,51 @@ pub struct MinecraftPackageManifest {
   pub package_type: MinecraftPackageType
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestArguments {
   pub game: Vec<MinecraftPackageManifestArgumentTypes>,
   pub jvm: Vec<MinecraftPackageManifestArgumentTypes>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(untagged)]
 pub enum MinecraftPackageManifestArgumentTypes {
   Simple(String),
   Ruled(MinecraftPackageManifestArgumentRuled),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestArgumentRuled {
   pub rules: Vec<MinecraftPackageManifestRuleTypes>,
   pub value: MinecraftPackageManifestArgumentRuledValueTypes,
 }
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(untagged)]
 pub enum MinecraftPackageManifestArgumentRuledValueTypes {
   Single(String),
   Multiple(Vec<String>)
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(untagged)]
 pub enum MinecraftPackageManifestRuleTypes {
   Os(MinecraftPackageManifestRuleOs),
   Features(MinecraftPackageManifestRuleFeatures),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestRuleOs {
   pub action: MinecraftPackageManifestRuleConditionAction,
   pub os: MinecraftPackageManifestRuleOsConditionValue,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestRuleFeatures {
   pub action: MinecraftPackageManifestRuleConditionAction,
   pub features: HashMap<String, bool>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum MinecraftPackageManifestRuleConditionAction {
   #[serde(rename = "allow")]
   Allow,
@@ -106,13 +106,13 @@ pub enum MinecraftPackageManifestRuleConditionAction {
   Disallow
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestRuleOsConditionValue {
   pub name: Option<MinecraftPackageManifestRuleConditionOsNames>,
   pub arch: Option<MinecraftPackageManifestRuleConditionArchNames>
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum MinecraftPackageManifestRuleConditionOsNames {
   #[serde(rename = "windows")]
   Windows,
@@ -122,7 +122,7 @@ pub enum MinecraftPackageManifestRuleConditionOsNames {
   Osx
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 /// This might be wrong. I couldn't find any info regarding all possible values
 pub enum MinecraftPackageManifestRuleConditionArchNames {
   #[serde(rename = "x86")]
@@ -131,7 +131,7 @@ pub enum MinecraftPackageManifestRuleConditionArchNames {
   X64,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestAssetIndex {
   pub id: String,
   pub sha1: String,
@@ -141,7 +141,7 @@ pub struct MinecraftPackageManifestAssetIndex {
   pub url: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestDownloads {
   pub client: MinecraftPackageManifestDownloadsEntry,
   pub client_mappings: MinecraftPackageManifestDownloadsEntry,
@@ -149,21 +149,21 @@ pub struct MinecraftPackageManifestDownloads {
   pub server_mappings: MinecraftPackageManifestDownloadsEntry,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestDownloadsEntry {
   pub sha1: String,
   pub size: u64,
   pub url: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestJavaVersion {
   component: MinecraftPackageManifestJavaVersionComponents,
   #[serde(rename = "majorVersion")]
   major_version: u16,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum MinecraftPackageManifestJavaVersionComponents {
   #[serde(rename = "java-runtime-alpha")]
   JavaRuntimeAlpha,
@@ -181,28 +181,28 @@ pub enum MinecraftPackageManifestJavaVersionComponents {
   MinecraftJavaExe,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(untagged)]
 pub enum MinecraftPackageManifestLibrary {
   Simple(MinecraftPackageManifestLibrarySimple),
   Ruled(MinecraftPackageManifestLibraryRuled)
 }
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestLibrarySimple {
   pub downloads: MinecraftPackageManifestLibraryDownloads,
   pub name: String,
 }
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestLibraryRuled {
   pub downloads: MinecraftPackageManifestLibraryDownloads,
   pub name: String,
   pub rules: Vec<MinecraftPackageManifestRuleOs>
 }
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestLibraryDownloads {
   pub artifact: MinecraftPackageManifestLibraryDownloadsEntry,
 }
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestLibraryDownloadsEntry {
   pub path: String,
   pub sha1: String,
@@ -210,18 +210,18 @@ pub struct MinecraftPackageManifestLibraryDownloadsEntry {
   pub url: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestLogging {
   pub client: MinecraftPackageManifestLoggingClient
 }
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestLoggingClient {
   pub argument: String,
   pub file: MinecraftPackageManifestLoggingFile,
   #[serde(rename = "type")]
   pub logging_type: String
 }
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MinecraftPackageManifestLoggingFile {
   pub id: String,
   pub sha1: String,
@@ -229,7 +229,7 @@ pub struct MinecraftPackageManifestLoggingFile {
   pub url: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum MinecraftPackageType {
   #[serde(rename = "release")]
   Release,
